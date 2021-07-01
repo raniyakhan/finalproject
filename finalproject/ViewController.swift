0baff5de0798bdf1f2814f95724299eca1253b75
raniyakhan <raniyakhan2005@gmail.com>
2021-07-01 10:59:34 -0400

    added sound

--- finalproject/ViewController.swift
+++ finalproject/ViewController.swift
@@ -6,14 +6,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func instrumentTapped(_ sender: UIButton) {
+        let selectedInstrument = sender.tag
+        switch selectedInstrument {
+        
+        case 1:
+            let pathtoSound = Bundle.main.path(forResource: "Alarm sound effects - modern alarm 1", ofType: "mp3")!
+            let url = URL(fileURLWithPath: pathtoSound)
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf : url)
+                audioPlayer?.play()
+            } catch {
+                
+            }
+        
+        default:
+            return
+    }
+    }
+    
 }
 
